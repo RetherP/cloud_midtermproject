82aab571aaf2eb3a41a46ff52501cf49cc6f47f1
RetherP <vbelya07@gmail.com>
2025-10-03 20:18:56 +0200

    FinalTest

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ def hello_word():
     return "<b>Hello Word</b>" \
     "<p>This is an test webpage for the project</p>" \
     "<br>" \
-    "<p>This is now running from the docker container.</p>" \
-    "<h1>Test2</h1>"
+    "<p>This is now running from the docker container.</p>"
 
 
 if __name__ == '__main__':
