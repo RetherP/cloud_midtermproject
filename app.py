8bb29571a2cb82f238a07218d8de7cd740845dfa
RetherP <vbelya07@gmail.com>
2025-10-03 18:45:29 +0200

    Test

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ def hello_word():
     return "<b>Hello Word</b>" \
     "<p>This is an test webpage for the project</p>" \
     "<br>" \
-    "<p>This is now running from the docker container</p>"
+    "<p>This is now running from the docker container </p>"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(port=80)
