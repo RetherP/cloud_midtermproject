6994b18010c9973242e7fd7b9e934ecec7a74384
RetherP <vbelya07@gmail.com>
2025-10-04 12:43:05 +0200

    Added bootsrap to the website

--- app.py
+++ app.py
@@ -6,19 +6,35 @@ def hello_word():
     return """
         <head>
             <title>Cloud Project Website</title>
-            <link rel="icon" href="img/cloud_icon.png">
+            <link rel="icon" href="/img/cloud_icon.png">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         </head>
         <body>
-            <center>
-                <h1> Hello Word </h1>
-                <p>This is a demo webpage for the midterm project</p>
-                <hr>
-                <p>This code is running from a Docker Container on Azure VM deployed via Github Actions </p>
-                <h2>The Deployment Status</h2>
-                <a href="https://github.com/RetherP/cloud_midtermproject/actions/workflows/main.yml">
-                    <img src="https://github.com/RetherP/cloud_midtermproject/actions/workflows/main.yml/badge.svg">            
-                </a>
-            </center>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1> Hello Word </h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p>This is a demo webpage for the midterm project</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">    
+                        <p>This code is running from a Docker Container on Azure VM deployed via Github Actions </p>
+                </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h2>The Deployment Status</h2>
+                        <a href="https://github.com/RetherP/cloud_midtermproject/actions/workflows/main.yml">
+                            <img src="https://github.com/RetherP/cloud_midtermproject/actions/workflows/main.yml/badge.svg">            
+                        </a>
+                </div>
+                </div>
+                
         </body>
     """
 
